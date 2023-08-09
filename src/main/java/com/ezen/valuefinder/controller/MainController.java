bb48dd550f9e82d5b8b87d5253dc480806f01d20
Lee-hye-won <hewon112311@gmail.com>
2023-08-09 16:43:06 +0900

    :heavy_plus_sign:add : 로그인 화면 구성

--- src/main/java/com/ezen/valuefinder/controller/MainController.java
+++ src/main/java/com/ezen/valuefinder/controller/MainController.java
@@ -10,5 +10,29 @@ public class MainController {
     public String main() {
         return "index";
     }
+    
+    @GetMapping(value = "/inquiry")
+    public String inquiry() {
+        return "inquirys/inquiry";
+    }
+    
+    @GetMapping(value = "/login2")
+    public String login() {
+        return "member/login";
+    }
 
+    @GetMapping(value = "/inquiryView")
+    public String upinquiry() {
+        return "inquirys/inquiryView";
+    }
+    
+    @GetMapping(value = "/report")
+    public String report() {
+        return "inquirys/report";
+    }
+
+    @GetMapping(value = "/publicbidDetails")
+    public String publicbidDetails() {
+    	return "details/publicbidDetails";
+    }
 }
