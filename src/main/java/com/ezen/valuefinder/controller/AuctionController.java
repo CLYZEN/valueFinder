7dafe6a435792d4207ef3e7312d379506ed2021f
cheonyeonsu <cheonyeonsu@gmail.com>
2023-08-10 13:04:55 +0900

    :heavy_plus_sign:add : 실시간경매,비공개경매view

---
+++ src/main/java/com/ezen/valuefinder/controller/AuctionController.java
@@ -0,0 +1,27 @@
+package com.ezen.valuefinder.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class AuctionController {
+
+	//실시간 경매 페이지
+		@GetMapping(value="/realtime")
+		public String auctionRealtime() {
+			
+			return "auction/realtime"; 
+		}
+		
+	//비공개 경매 페이지
+		@GetMapping(value="/sealedbid")
+		public String auctionSealedbid() {
+			
+			return "auction/sealedbid"; 
+		}
+		
+}
