39b8ffd6588e85b6849f3bf3bf2a8d392a17df4b
hanzoeun <dorlgodfhd@naver.com>
2023-08-14 16:58:21 +0900

    :heavy plus sign:add관리자페이지구현

--- src/main/java/com/ezen/valuefinder/controller/AdminController.java
+++ src/main/java/com/ezen/valuefinder/controller/AdminController.java
@@ -7,7 +7,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdminController {
 	@GetMapping(value = "/admin")
 	public String adminM() {
-					return "admin/main";
-				}
-	
+		return "admin/main";
+	}
+
+	@GetMapping(value = "/admin/itemList")
+	public String adminitemlist() {
+		return "admin/itemList";
+	}
+
+	@GetMapping(value = "/admin/customerList")
+	public String admincustomerList() {
+		return "admin/customerList";
+	}
+
+	@GetMapping(value = "/admin/itemreportList")
+	public String adminitemreportList() {
+		return "admin/itemreportList";
+	}
+
+	@GetMapping(value = "/admin/customerReportList")
+	public String admincustomerReportList() {
+		return "admin/customerReportList";
+	}
+
+	@GetMapping(value = "/admin/customerStatus")
+	public String admincustomerStatus() {
+		return "admin/customerStatus";
+	}
+
+	@GetMapping(value = "/admin/itemStatus")
+	public String adminitemStatus() {
+		return "admin/itemStatus";
+	}
+
 }
