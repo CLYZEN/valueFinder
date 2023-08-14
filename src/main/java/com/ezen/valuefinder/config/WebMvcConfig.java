ce98ac9e154737f61320f37efa6921318ec5a20f
clyzen <jimuny01@gmail.com>
2023-08-14 14:59:33 +0900

    :heavy_plus_sign:add : 아이디 저장 완료 :ghost:refactor : DB정보 숨김, 정적파일 읽어오는 방법 변경

--- src/main/java/com/ezen/valuefinder/config/WebMvcConfig.java
+++ src/main/java/com/ezen/valuefinder/config/WebMvcConfig.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.net.URL;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    String uploadPath = "file:../../itemImg/";
+
+    ClassLoader classLoader = getClass().getClassLoader();
+    URL url = classLoader.getResource("");
+    String uploadPath = url.getPath();
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/itemImg");
+                .addResourceLocations(uploadPath);
     }
 }
