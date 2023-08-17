bcb60ed8c2b7e12d68dfa1d494f80ff63c76c455
clyzen <jimuny01@gmail.com>
2023-08-17 15:10:39 +0900

    :heavy_plus_sign:add : 회원탈퇴 기능 완료

---
+++ src/main/java/com/ezen/valuefinder/repository/MemberOutRepository.java
@@ -0,0 +1,7 @@
+package com.ezen.valuefinder.repository;
+
+import com.ezen.valuefinder.entity.MemberOut;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberOutRepository extends JpaRepository<MemberOut, Long> {
+}
