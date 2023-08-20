932ba772778d03e22e4be16239b6fc05d0d724e7
cheonyeonsu <cheonyeonsu@gmail.com>
2023-08-21 08:24:12 +0900

    :pencil:chore : 찜 기능 수정중

---
+++ src/main/java/com/ezen/valuefinder/controller/LikeController.java
@@ -0,0 +1,27 @@
+package com.ezen.valuefinder.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.ezen.valuefinder.dto.LikeRequestDto;
+
+
+public class LikeController {
+	 private final LikeServiceImpl likeService;
+	 
+	// 경매 글에 찜 추가
+	    @PostMapping("/addLike")
+	    public void addLike(@RequestBody LikeRequestDto likeRequest) {
+	        likeService.addLike(likeRequest.getAuctionNo(), likeRequest.getMemberId());
+	    }
+
+	 // 경매 글에 찜 취소
+	    @DeleteMapping("/deleteLike")
+	    public void deleteLike(@RequestBody LikeRequestDto likeRequest) {
+	        likeService.deleteLike(likeRequest.getAuctionNo(), likeRequest.getMemberId());
+	    }
+
+	}
+	 
+}
