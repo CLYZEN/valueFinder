d784ebd64d315b665aca1cbf5358498037dfb9f8
clyzen <jimuny01@gmail.com>
2023-08-21 11:43:08 +0900

    :heavy_plus_sign:add : async 추가

--- src/main/java/com/ezen/valuefinder/ValuefinderApplication.java
+++ src/main/java/com/ezen/valuefinder/ValuefinderApplication.java
@@ -2,6 +2,7 @@ package com.ezen.valuefinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 public class ValuefinderApplication {
