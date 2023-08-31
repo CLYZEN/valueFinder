15a4760960ca03daf6a95ee1306404a61ddaafba
hanzoeun <dorlgodfhd@naver.com>
2023-08-31 17:17:45 +0900

    :heavy_plus_sign:add문의하기상세페이지구현

--- src/main/java/com/ezen/valuefinder/repository/MemberOutRepository.java
+++ src/main/java/com/ezen/valuefinder/repository/MemberOutRepository.java
@@ -1,7 +1,9 @@
 package com.ezen.valuefinder.repository;
 
+import com.ezen.valuefinder.entity.Member;
 import com.ezen.valuefinder.entity.MemberOut;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberOutRepository extends JpaRepository<MemberOut, Long> {
+	MemberOut findByMember(Member member);
 }
